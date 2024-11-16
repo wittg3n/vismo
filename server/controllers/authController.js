2a73fe9e4d1166c8c58a2a30cb64fc54ad080bd4
wittg3n <wittg3ns@gmail.com>
2024-11-17 02:09:40 +0330

    shit

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -9,9 +9,14 @@ export const signUp = async (req, res) => {
     console.log({ username, email, password });
 
     try {
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ message: 'User already exists' });
+        const existingEmail = await User.findOne({ email });
+        if (existingEmail) {
+            return res.status(400).json({ message: 'این ایمیل قبلاً ثبت شده است.' });
+        }
+
+        const existingUsername = await User.findOne({ username });
+        if (existingUsername) {
+            return res.status(400).json({ message: 'این نام کاربری قبلاً استفاده شده است.' });
         }
 
         const newUser = new User({ username, email, password });
@@ -25,25 +30,44 @@ export const signUp = async (req, res) => {
         );
 
         res.status(201).json({
-            message: 'User registered successfully',
+            message: 'حساب کاربری با موفقیت ساخته شد',
             token
         });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Error registering user' });
+        res.status(500).json({ message: 'خطا در ثبت نام رخ داده است.' });
+    }
+};
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'کاربری با این ایمیل پیدا نشد.' });
+        }
+
+        const isPasswordValid = await user.comparePassword(password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: 'رمز عبور اشتباه است.' });
+        }
+        //TODO make two seporate jwt check helpers
+        const token = jwt.sign(
+            { id: user._id, email: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: '1d' }
+        );
+
+        res.status(200).json({
+            message: 'ورود با موفقیت انجام شد',
+            token
+        });
+    } catch (error) {
+        console.log(asdas);
+        res.status(500).json({ message: 'خطا در ورود رخ داده است. لطفاً دوباره تلاش کنید.' });
     }
 };
 
-export const login = (req, res) => {
-    const user = req.user;
-    console.log(user);
-    const token = jwt.sign(
-        { id: user._id, email: user.email },
-        process.env.JWT_SECRET,
-        { expiresIn: '1d' }
-    );
-    res.json({ message: 'Login successful', token });
-};
 
 export const deleteAccount = async (req, res) => {
     try {
